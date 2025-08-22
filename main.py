b8b14321e5f3dbb1c4c98ffa9aa857afd9154dba
ashcodes3014 <aashishkumarak7065@gmail.com>
2025-08-22 23:46:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from src.model import ask_crypto_bot
 
-
 app = FastAPI(title="Crypto Chat Agent")
 
 class ChatRequest(BaseModel):
     question: str
-    context: list[dict] = []
 
 @app.post("/crypto-chat")
-async def crypto_chat(request: ChatRequest) :
-    response = ask_crypto_bot(request.context,request.question)
+async def crypto_chat(request: ChatRequest):
+    response = ask_crypto_bot(request.question)
     return response
